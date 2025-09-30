dd75178303ce00c5da4393f625a899cb1c7be63a
25LP4018 25LP4018 <25LP4018@G301-CC0190.local>
2025-09-30 14:14:18 +0800

    postandget

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,79 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const Home = () => {
-  useEffect(() => {
+  const [newTask, setNewTask] = useState("");
+  const [tasks, setTasks] = useState<{ id: string; name: string }[]>([]);
+  // console.log(newTask, "newTask");
+  // console.log(tasks, "tasks");
+
+  async function createNewTask() {
+    newTask !== "" &&
+      (await fetch("http://localhost:3000/tasks", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name: newTask }),
+      }));
+    loadTasks();
+    setNewTask("");
+  }
+
+  function loadTasks() {
     fetch("http://localhost:3000/tasks")
       .then((res) => res.json())
-      .then((data) => console.log(data, "data"));
+      .then((data) => {
+        setTasks(data);
+      });
+  }
+
+  function deleteTask(id: string) {
+    fetch(`http://localhost:3000/tasks/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ name: newTask }),
+    });
+    // loadTasks();
+
+    // .then((res) => {})
+    // .then((data) => {})
+    // .catch((error) => {});
+  }
+  useEffect(() => {
+    loadTasks();
   }, []);
-  return <div>page</div>;
+  return (
+    <div className="flex justify-center mt-100">
+      <div className="card w-96 bg-base-100 card-lg shadow-lg">
+        <div className="card-body">
+          <h2 className="card-title">To-Do</h2>
+          <div className="flex">
+            <input
+              className="input mr-4"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+            ></input>
+            <button onClick={createNewTask} className="btn">
+              Add
+            </button>
+          </div>
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              className="card border border-base-300 p-2 mt-2 flex flex-row justify-between"
+            >
+              <div>
+                {task.name} {task.id}
+              </div>
+              <button onClick={() => deleteTask(task.id)}>X</button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 export default Home;
