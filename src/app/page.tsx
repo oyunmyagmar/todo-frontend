d4ce72081dc4d72dd9256613404d86bdffada98c
25LP4018 25LP4018 <25LP4018@G301-CC0190.local>
2025-10-03 13:43:35 +0800

    ADEED

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,22 +4,19 @@ import { CiEdit } from "react-icons/ci";
 import { BsPlusLg } from "react-icons/bs";
 import { MdDeleteOutline } from "react-icons/md";
 import { RxHamburgerMenu } from "react-icons/rx";
-
 import { SideBar } from "@/components";
-// import { AddedTasks } from "@/components";
 
+type Task = { id: string; name: string; isDone: boolean };
 const Home = () => {
   const [newTask, setNewTask] = useState("");
-  const [tasks, setTasks] = useState<
-    { id: string; name: string; isDone: boolean }[]
-  >([]);
-  // console.log(newTask, "newTask");
-  // console.log(tasks, "tasks");
-  // const [filteredStatus, setFilteredStatus] = useState("All");
-  // console.log(filteredStatus, "filteredStatus");
+  const [tasks, setTasks] = useState<Task[]>([]);
+  const [status, setStatus] = useState("All");
+
+  useEffect(() => {
+    loadTasks();
+  }, [status]);
 
   async function createNewTask() {
-    // setFilteredStatus("Active");
     await fetch("http://localhost:3000/tasks", {
       method: "POST",
       headers: {
@@ -32,7 +29,7 @@ const Home = () => {
   }
 
   function loadTasks() {
-    fetch("http://localhost:3000/tasks")
+    fetch(`http://localhost:3000/tasks?status=${status}`)
       .then((res) => res.json())
       .then((data) => {
         setTasks(data);
@@ -40,8 +37,6 @@ const Home = () => {
   }
 
   async function deleteTask(id: string) {
-    // console.log("delete ajilaa");
-    // alert(id);
     if (confirm("Are you sure you want to delete this task?")) {
       await fetch(`http://localhost:3000/tasks/${id}`, {
         method: "DELETE",
@@ -50,7 +45,7 @@ const Home = () => {
     }
   }
 
-  async function editTask(task: { id: string; name: string }) {
+  async function editTask(task: Task) {
     const newName = prompt("Edit", task.name);
     if (newName) {
       await fetch(`http://localhost:3000/tasks/${task.id}`, {
@@ -64,155 +59,97 @@ const Home = () => {
     }
   }
 
-  async function updateTask(id: string) {
-    // setFilteredStatus("Completed");
-    await fetch(`http://localhost:3000/check/tasks/${id}`, {
-      method: "PUT",
+  async function toggleCheckBox(id: string) {
+    await fetch(`http://localhost:3000/tasks/${id}/check`, {
+      method: "PATCH",
     });
     loadTasks();
   }
 
-  // async function filterTasks(btn: string) {
-  //   // setFilteredStatus(btn);
-
-  //   await fetch(`http://localhost:3000/tasks/${btn}`, {
-  //     method: "PUT",
-  //   });
-  //   loadTasks();
-  // }
-
-  useEffect(() => {
-    loadTasks();
-  }, []);
-  const filterBtn = ["All", "Active", "Completed"];
+  async function deleteAllCompleted() {
+    const filtered = tasks.filter((el) => el.isDone);
+  }
 
   return (
-    <div className="w-screen h-screen flex bg-gray-100">
-      {/* <div className="w-100 h-screen bg-white px-5 ">
-        <h1>Sidebar</h1> */}
-      {/* {filterBtn.map((btn) => (
-          <button className="btn w-full" onClick={() => filterTasks(btn)}>
-            {btn}
-          </button>
-        ))} */}
-      {/* </div> */}
-      <div className="mx-10">
-        <button className="btn">
-          <RxHamburgerMenu />
-        </button>
-        <div className="card w-full bg-base-100 card-lg shadow-lg mt-10">
-          <div className="card-body">
-            <h2 className="card-title">To-Do</h2>
-            <div className="w-full flex">
-              <input
-                type="text"
-                placeholder="Add a task..."
-                className="input mr-4 w-full"
-                value={newTask}
-                onChange={(e) => setNewTask(e.target.value)}
-                onKeyDown={(e) => e.key == "Enter" && createNewTask()}
-              />
-              <button
-                disabled={!newTask}
-                onClick={createNewTask}
-                className="btn btn-info btn-soft"
+    <div className="w-screen h-full px-10 mt-5 mb-20">
+      <button className="btn">
+        <RxHamburgerMenu />
+      </button>
+
+      <div className="card w-150 bg-base-100 shadow-sm mt-5">
+        <div className="card-body">
+          <h2 className="card-title">To-Do</h2>
+          <div className="w-full flex">
+            <input
+              type="text"
+              placeholder="Add a task..."
+              className="input mr-4 flex-1"
+              value={newTask}
+              onChange={(e) => setNewTask(e.target.value)}
+              onKeyDown={(e) => e.key == "Enter" && createNewTask()}
+            />
+            <button
+              disabled={!newTask}
+              onClick={createNewTask}
+              className="btn btn-info btn-soft rounded-xs"
+            >
+              <BsPlusLg size={26} />
+            </button>
+          </div>
+
+          <div role="tablist" className="tabs tabs-box mt-2">
+            {["All", "Active", "Completed"].map((el) => (
+              <a
+                key={el}
+                role="tab"
+                className={`tab flex-1 ${status === el ? "tab-active" : ""}`}
+                onClick={() => setStatus(el)}
               >
-                <BsPlusLg size={26} />
-              </button>
-            </div>
-            {tasks.map((task) => (
-              // <AddedTasks
-              //   task={task}
-              //   delete={() => deleteTask(task.id)}
-              //   edit={() => editTask(task)}
-              //   update={() => updateTask(task.id)}
-              // />
-              <div
-                key={task.id}
-                className={`card border border-base-300 p-4 mt-2 flex-row justify-between ${
-                  task.isDone && "hidden"
-                }`}
-              >
-                <div className="w-70 flex gap-2 items-center">
-                  <input
-                    checked={task.isDone}
-                    type="checkbox"
-                    onChange={() => updateTask(task.id)}
-                    className="accent-blue-300"
-                  />
-                  <div className={`${task.isDone && "line-through"} truncate`}>
-                    {task.name}
-                  </div>
-                </div>
-                <div className="flex gap-2 items-center">
-                  <button
-                    className={`btn btn-ghost btn-square ${
-                      task.isDone && "hidden"
-                    }`}
-                    onClick={() => editTask(task)}
-                  >
-                    <CiEdit size={28} />
-                  </button>
-                  <button
-                    className="btn btn-error btn-soft btn-square hover:"
-                    onClick={() => deleteTask(task.id)}
-                  >
-                    <MdDeleteOutline size={20} />
-                  </button>
+                {el}
+              </a>
+            ))}
+          </div>
+
+          {tasks.map((task) => (
+            <div
+              key={task.id}
+              className={`card border border-base-300 p-4 mt-2 flex-row justify-between`}
+            >
+              <div className="flex gap-2 items-center">
+                <input
+                  type="checkbox"
+                  onChange={() => toggleCheckBox(task.id)}
+                  defaultChecked={task.isDone}
+                  className="checkbox checkbox-success checkbox-xs rounded-xs"
+                />
+
+                <div className={`${task.isDone && "line-through"} flex-1`}>
+                  {task.name}
                 </div>
               </div>
-            ))}
-            <div>
-              <button
-                className={`btn btn-neutral ${tasks.map((task) =>
-                  task.isDone ? "block" : "hidden"
-                )}`}
-              >
-                Completed {tasks.filter((el) => el.isDone).length} out of{" "}
-                {tasks.length}
-              </button>
-              {tasks.map(
-                (task) =>
-                  task.isDone && (
-                    <div
-                      key={task.id}
-                      className="card border border-base-300 p-4 mt-2 flex-row justify-between"
-                    >
-                      <div className="w-70 flex gap-2 items-center">
-                        <input
-                          checked={task.isDone}
-                          type="checkbox"
-                          onChange={() => updateTask(task.id)}
-                        />
-                        <div
-                          className={`${
-                            task.isDone && "line-through"
-                          } truncate`}
-                        >
-                          {task.name}
-                        </div>
-                      </div>
-                      <div className="flex gap-2 items-center">
-                        <button
-                          className={`btn btn-ghost btn-square ${
-                            task.isDone && "hidden"
-                          }`}
-                          onClick={() => editTask(task)}
-                        >
-                          <CiEdit size={28} />
-                        </button>
-                        <button
-                          className="btn btn-error btn-soft btn-square"
-                          onClick={() => deleteTask(task.id)}
-                        >
-                          <MdDeleteOutline size={20} />
-                        </button>
-                      </div>
-                    </div>
-                  )
-              )}
+
+              <div className="flex gap-2 items-center">
+                <button
+                  className={`btn btn-ghost btn-square ${
+                    task.isDone && "hidden"
+                  } rounded-xs`}
+                  onClick={() => editTask(task)}
+                >
+                  <CiEdit size={28} />
+                </button>
+
+                <button
+                  className="btn btn-error btn-soft btn-square rounded-xs"
+                  onClick={() => deleteTask(task.id)}
+                >
+                  <MdDeleteOutline size={20} />
+                </button>
+              </div>
             </div>
-          </div>
+          ))}
+          <button className="btn mt-2" onClick={deleteAllCompleted}>
+            Clear All Completed Tasks
+          </button>
         </div>
       </div>
     </div>
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,2 +1,1 @@
-export * from "./AddedTasks";
 export * from "./SideBar";
